3fbe76d6e3794504be6d3a1e49278f58c6cb65d7
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-06-30 22:09:49 -0500

    hid firebase api keys

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from "react-router-dom"
 import App from './App'
 import './index.css'
+import { FBapiKey, FBauthDomain, FBdatabaseURL, FBprojectId, FBstorageBucket, FBmessagingSenderId, FBappId } from './config';
 import * as firebase from 'firebase/app'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDdWb-iSM10lu7zdnG_1Wd-bZLrwjLZiPs",
-    authDomain: "fe-capstone-auth.firebaseapp.com",
-    databaseURL: "https://fe-capstone-auth.firebaseio.com",
-    projectId: "fe-capstone-auth",
-    storageBucket: "fe-capstone-auth.appspot.com",
-    messagingSenderId: "510336772453",
-    appId: "1:510336772453:web:36734bcb2e6f346b"
+    apiKey: FBapiKey,
+    authDomain: FBauthDomain,
+    databaseURL: FBdatabaseURL,
+    projectId: FBprojectId,
+    storageBucket: FBstorageBucket,
+    messagingSenderId: FBmessagingSenderId,
+    appId: FBappId
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
